b68525304b6a7ebe8f85135d4f73488e2305d2a9
impreza555 <alex.erjomin@outlook.com>
2021-07-16 18:04:28 +0300

    Сделал упражнение 1.5

--- exercise_5.py
+++ exercise_5.py
@@ -1,3 +1,31 @@
 """
 5. Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят и сколько между ними находится букв.
 """
+
+
+def positions_alphabet():
+    while True:
+        letter1 = input('Введите первую букву латинского алфавита ').lower()
+        if letter1.isalpha():
+            if ord(letter1) not in range(97, 123):
+                print('Заданная буква не в диапазоне латинского алфавита')
+            else:
+                break
+        else:
+            print('Некорректный ввод')
+    while True:
+        letter2 = input('Введите вторую букву латинского алфавита ').lower()
+        if letter2.isalpha():
+            if ord(letter2) not in range(97, 123):
+                print('Заданная буква не в диапазоне латинского алфавита')
+            else:
+                break
+        else:
+            print('Некорректный ввод')
+    res1 = ord(letter1) - 96
+    res2 = ord(letter2) - 96
+    print(f'Введённые буквы стоят на {res1} и {res2} местах в алфавите\nМежду ними {res2 - res1 - 1} букв')
+
+
+if __name__ == "__main__":
+    positions_alphabet()
