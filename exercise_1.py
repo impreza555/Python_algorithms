036af3d1a06a4fe00688a5abf0a0ae8bf9835e52
impreza555 <alex.erjomin@outlook.com>
2021-07-22 18:38:45 +0300

    Сделал упражнение 2.1 и 2.4

--- exercise_1.py
+++ exercise_1.py
@@ -7,3 +7,40 @@
 сообщать ему об ошибке и снова запрашивать знак операции.
 Также сообщать пользователю о невозможности деления на ноль, если он ввел 0 в качестве делителя.
 """
+
+
+def calculator():
+    while True:
+        try:
+            first_num = float(input('Введите первое число '))
+            break
+        except ValueError:
+            print('Вы должны ввести число')
+    while True:
+        try:
+            second_num = float(input('Введите второе число '))
+            break
+        except ValueError:
+            print('Вы должны ввести число')
+    while True:
+        operator = input('Введите знак операции: "+", "-", "*", "/", либо введите "0" для завершения программы ')
+        if operator == '0':
+            quit()
+        if second_num == 0.0 and operator == "/":
+            print('Делить на "0" нельзя... В этом измерении...')
+            continue
+        if operator == "+" or operator == "-" or operator == "*" or operator == "/":
+            break
+    if operator == "+":
+        result = first_num + second_num
+    elif operator == "-":
+        result = first_num - second_num
+    elif operator == "*":
+        result = first_num * second_num
+    elif operator == "/":
+        result = first_num / second_num
+    return f'{first_num} {operator} {second_num} = {result}'
+
+
+if __name__ == '__main__':
+    print(calculator())
