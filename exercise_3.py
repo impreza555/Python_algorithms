543ffd327392be0c56df6b72b1b16401b11c530e
impreza555 <alex.erjomin@outlook.com>
2021-07-24 02:42:20 +0300

    Сделал упражнение 3.3.

--- exercise_3.py
+++ exercise_3.py
@@ -1,3 +1,19 @@
+import random
+
 """
 3. В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
 """
+
+
+random.seed(12)
+
+
+def min_it_is_max():
+    arr = [random.randint(-100, 100) for i in range(20)]
+    print(f'В массиве {arr}, минимальный элемент {min(arr)}, максимальный элемент {max(arr)}')
+    arr[arr.index(min(arr))], arr[arr.index(max(arr))] = arr[arr.index(max(arr))], arr[arr.index(min(arr))]
+    print(f'Меняем местами {min(arr)} и {max(arr)}, получаем\n{arr}')
+
+
+if __name__ == '__main__':
+    min_it_is_max()
