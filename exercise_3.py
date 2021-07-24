b49f6f1cc5f363698d0007b57e6644f74b2a76e4
impreza555 <alex.erjomin@outlook.com>
2021-07-24 19:14:43 +0300

    Сделал упражнение 3.4.

--- exercise_3.py
+++ exercise_3.py
@@ -4,14 +4,19 @@ import random
 3. В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
 """
 
-random.seed(12)
+
+# random.seed(12)
 
 
 def min_it_is_max():
-    arr = [random.randint(-100, 100) for i in range(20)]
-    print(f'В массиве {arr}, минимальный элемент {min(arr)}, максимальный элемент {max(arr)}')
-    arr[arr.index(min(arr))], arr[arr.index(max(arr))] = arr[arr.index(max(arr))], arr[arr.index(min(arr))]
-    print(f'Меняем местами {min(arr)} и {max(arr)}, получаем\n{arr}')
+    arr = [random.randint(-100, 100) for i in range(15)]
+    min_el = min(arr)
+    max_el = max(arr)
+    print(f'В массиве {arr}, минимальный элемент {min_el}, максимальный элемент {max_el}')
+    arr[arr.index(min_el)] = max_el
+    arr[arr.index(max_el)] = min_el
+    # arr[arr.index(min(arr))], arr[arr.index(max(arr))] = arr[arr.index(max(arr))], arr[arr.index(min(arr))]
+    print(f'Меняем местами {min_el} и {max_el}, получаем\n{arr}')
 
 
 if __name__ == '__main__':
