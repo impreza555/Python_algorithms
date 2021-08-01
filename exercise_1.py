26f10ebb758b9eb9fec9bd1528e1dc94552ab7c9
impreza555 <alex.erjomin@outlook.com>
2021-08-01 16:56:14 +0300

    Сделал упражнение 4.1.

--- exercise_1.py
+++ exercise_1.py
@@ -11,9 +11,15 @@ import exercise_4_2 as geometricProgression
 """
 
 
-cProfile.run('print(geometricProgression.sum(500, 1))')
+cProfile.run('print(geometricProgression.sum(10, 1))')
 print(
-    f"Время выполнения: {timeit.timeit('print(geometricProgression.sum(500, 1))', setup='import exercise_4_2 as geometricProgression', number=100)}")
+    f"Время выполнения: {timeit.timeit('print(geometricProgression.sum(10, 1))', setup='import exercise_4_2 as geometricProgression', number=100)}")
+cProfile.run('print(geometricProgression.sum(100, 1))')
+print(
+    f"Время выполнения: {timeit.timeit('print(geometricProgression.sum(100, 1))', setup='import exercise_4_2 as geometricProgression', number=100)}")
+cProfile.run('print(geometricProgression.sum(990, 1))')
+print(
+    f"Время выполнения: {timeit.timeit('print(geometricProgression.sum(990, 1))', setup='import exercise_4_2 as geometricProgression', number=100)}")
 print('*' * 40)
 print()
 
@@ -25,8 +31,22 @@ def sum(n, el1):
         el1 *= q
     return sum0
 """
-# Линейная сложность: алгоритм проходит один раз от 0 до n прибавляя к сумме следующий элемент
 
+"""
+0.666015625
+         5 function calls in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 exercise_4_2.py:8(sum)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+        
+Время выполнения: 0.0017154000000000058
+"""
 """
 0.6666666666666667
          5 function calls in 0.000 seconds
@@ -40,12 +60,35 @@ def sum(n, el1):
         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
         
-Время выполнения: 0.011411399999999988
+Время выполнения: 0.0031104999999999883
 """
+"""
+0.6666666666666667
+         5 function calls in 0.000 seconds
 
-cProfile.run('print(geometricProgression.sum_rec(500, 1))')
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+        1    0.000    0.000    0.000    0.000 exercise_4_2.py:8(sum)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+        
+Время выполнения: 0.020294900000000005
+"""
+# Линейная сложность: алгоритм проходит один раз от 0 до n прибавляя к сумме следующий элемент
+
+
+cProfile.run('print(geometricProgression.sum_rec(10, 1))')
 print(
-    f"Время выполнения: {timeit.timeit('print(geometricProgression.sum_rec(500, 1))', setup='import exercise_4_2 as geometricProgression', number=100)}")
+    f"Время выполнения: {timeit.timeit('print(geometricProgression.sum_rec(10, 1))', setup='import exercise_4_2 as geometricProgression', number=100)}")
+cProfile.run('print(geometricProgression.sum_rec(100, 1))')
+print(
+    f"Время выполнения: {timeit.timeit('print(geometricProgression.sum_rec(100, 1))', setup='import exercise_4_2 as geometricProgression', number=100)}")
+cProfile.run('print(geometricProgression.sum_rec(990, 1))')
+print(
+    f"Время выполнения: {timeit.timeit('print(geometricProgression.sum_rec(990, 1))', setup='import exercise_4_2 as geometricProgression', number=100)}")
 print('*' * 40)
 
 """
@@ -57,22 +100,52 @@ def sum_rec(n, el1):
     el1 *= q
     return sum1 + sum_rec(n - 1, el1)
 """
-# Квадратичная сложность: функция рекурсивно вызывает саму себя столько раз, сумму скольких элементов требуется посчитать.
 
 """
-0.6666666666666666
-         504 function calls (5 primitive calls) in 0.002 seconds
+0.666015625
+         14 function calls (5 primitive calls) in 0.000 seconds
 
    Ordered by: standard name
 
    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-        1    0.000    0.000    0.002    0.002 <string>:1(<module>)
-    500/1    0.002    0.000    0.002    0.002 exercise_4_2.py:17(sum_rec)
-        1    0.000    0.000    0.002    0.002 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+     10/1    0.000    0.000    0.000    0.000 exercise_4_2.py:17(sum_rec)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
        
-Время выполнения: 0.029903999999999986
+Время выполнения: 0.002571999999999991
 """
+"""
+0.6666666666666666
+         104 function calls (5 primitive calls) in 0.000 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+    100/1    0.000    0.000    0.000    0.000 exercise_4_2.py:17(sum_rec)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+        
+Время выполнения: 0.007070300000000002
+"""
+"""
+0.6666666666666666
+         994 function calls (5 primitive calls) in 0.006 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.006    0.006 <string>:1(<module>)
+    990/1    0.006    0.000    0.006    0.006 exercise_4_2.py:17(sum_rec)
+        1    0.000    0.000    0.006    0.006 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+        
+Время выполнения: 0.05928520000000001
+"""
+# Линейная сложность: функция рекурсивно вызывает саму себя столько раз, сумму скольких элементов требуется посчитать.
 
 # Вывод: хотя разница, в данном примере, незначительна, всё же, первый алгоритм работает быстрее.
