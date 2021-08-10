f0da89dac649abf51fb3f5d23432810498953e14
impreza555 <alex.erjomin@outlook.com>
2021-08-10 22:56:46 +0300

    Сделал упражнение 7.2

--- exercise_2.py
+++ exercise_2.py
@@ -1,4 +1,37 @@
+import random
+
 """
 2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
 """
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        left = arr[:len(arr) // 2]
+        right = arr[len(arr) // 2:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    return arr
+
+
+a = [round(random.uniform(0, 50), 3) for i in range(20)]
+print(f'Исходный массив:\n{a}')
+print(f'Отсортированный массив:\n{merge_sort(a)}')
