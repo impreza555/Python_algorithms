2971a4523b917144608df8c35ef8434b6a22a342
impreza555 <alex.erjomin@outlook.com>
2021-08-13 23:04:32 +0300

    Сделал упражнение 8.2

--- exercise_2.py
+++ exercise_2.py
@@ -1,6 +1,59 @@
+import heapq
+from collections import Counter
+from collections import namedtuple
+
 """
 2. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
 """
 
-def haffman():
-    pass
+
+class Node(namedtuple('Node', ['left', 'right'])):
+    def walk(self, code, acc):
+        self.left.walk(code, acc + '0')
+        self.right.walk(code, acc + '1')
+
+
+class Leaf(namedtuple('Leaf', ['char'])):
+    def walk(self, code, acc):
+        code[self.char] = acc or '0'
+
+
+def huffman_encode(s):
+    h = []
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+    heapq.heapify(h)
+    count = len(h)
+    while len(h) > 1:
+        freq1, _count1, left = heapq.heappop(h)
+        freq2, _count2, right = heapq.heappop(h)
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+    code = {}
+    if h:
+        [(_freq, _count, root)] = h
+        root.walk(code, '')
+        return code
+
+
+def huffman_decode(encoded, code):
+    sx = []
+    enc_ch = ''
+    for ch in encoded:
+        enc_ch += ch
+        for dec_ch in code:
+            if code.get(dec_ch) == enc_ch:
+                sx.append(dec_ch)
+                enc_ch = ''
+                break
+    return ''.join(sx)
+
+
+if __name__ == '__main__':
+    s = input('Введите строку ')
+    code = huffman_encode(s)
+    encoded = ''.join(code[ch] for ch in s)
+    for ch in sorted(code):
+        print('{}: {}'.format(ch, code[ch]))
+    print(f'Исходная строка: {huffman_decode(encoded, code)}')
+    print(f'Закодированная строка: {encoded}')
